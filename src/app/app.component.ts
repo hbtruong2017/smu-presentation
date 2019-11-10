2a085e288e47c71b0376c74a765857fb32c297a2
Bang <hbtruong.2017@sis.smu.edu.sg>
2019-11-11 05:47:20 +0800

    Upload image and audio files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'presentation';
+
+  howdareyou() {
+    let audio = new Audio("../assets/howdareyou.mp3");
+    audio.play();
+  }
+
+  playSound() {
+    let audio = new Audio("../assets/gretaspeech.mp3");
+    audio.play();
+  }
+
+  goToVideo() {
+    window.location.href = "https://youtu.be/haewHZ8ubKA";
+  }
+
+  goToTranscript() {
+    window.location.href = "https://www.npr.org/2019/09/23/763452863/transcript-greta-thunbergs-speech-at-the-u-n-climate-action-summit"
+  }
 }
+
+
